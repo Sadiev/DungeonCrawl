03b67bbb251e6a52f3626764c57139bf85b7d748
bmlegge <36506641+bmlegge@users.noreply.github.com>
2019-09-12 16:05:26 -0400

    Add files via upload

---
+++ DungeonCrawl-master/DungeonCrawl-master/DungeonCrawl/DungeonCrawl-master/DungeonCrawl-master/code/DungeonCrawl/DungeonCrawl/Arrays.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonCrawl
+{
+    class Arrays
+    {
+        static void arrays()
+        {
+            const int ROOM_SIZE = 5;
+            const int WEAP_SIZE = 4;
+            const int POT_SIZE = 2;
+            const int TREAS_SIZE = 3;
+
+            string[] roomArray = new string[ROOM_SIZE];
+            roomArray[0] = "Entrance";
+            roomArray[1] = "Hallway";
+            roomArray[2] = "Office";
+            roomArray[3] = "Barracks";
+            roomArray[4] = "Laboratory";
+
+            string[] weapArray = new string[WEAP_SIZE];
+            weapArray[0] = "Two-Handed Sword";
+            weapArray[1] = "Dagger";
+            weapArray[2] = "Wand";
+            weapArray[3] = "Bow";
+
+            string[] potArray = new string[POT_SIZE];
+            potArray[0] = "Health Potion";
+            potArray[1] = "Armor Potion";   //Increase HP by certain amount.
+
+            string[] treasArray = new string[TREAS_SIZE];
+            treasArray[0] = "Gold";
+            treasArray[1] = "Armor";
+            treasArray[2] = "Gems";
+        }
+    }
+}
---
+++ DungeonCrawl-master/DungeonCrawl-master/DungeonCrawl/DungeonCrawl-master/DungeonCrawl-master/code/DungeonCrawl/DungeonCrawl/Class.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonCrawl
+{
+    class Class
+    {
+        List<string> mobs = new List<string>();
+        List<string> items = new List<string>();
+        public static int location = 0;
+
+
+        public static void Move(string direction, string[] rooms)
+        {
+            switch (direction)
+            {
+                case "north":
+                    if (location < rooms.Length)
+                        location++;                   
+                    break;
+                case "south":
+                    if (location > 0)
+                        location--;
+                    break;
+                default:
+                    Console.WriteLine("Not a valid location");
+                    break;
+            }
+            Console.WriteLine($"Your location is {rooms[location]}");
+
+        }
+    }
+}
