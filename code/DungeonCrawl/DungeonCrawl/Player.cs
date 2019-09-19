265a98ae15660ddc0a20294c9bf460ea8ffeac23
Bradley Legge <leggeb9867@ad.faytechcc.edu>
2019-09-19 10:51:03 -0400

    Added Player

---
+++ code/DungeonCrawl/DungeonCrawl/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonCrawl
+{
+    public class Player
+    {
+        public string PlayerName { get; set; }
+        public string Password { get; set; }
+        public string Class { get; set; }
+        public string Race { get; set; }
+    }
+}
+
+
