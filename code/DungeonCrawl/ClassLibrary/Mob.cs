48fd4bb331638bd80783a93ca335d3db0478a9bf
Bradley Legge <leggeb9867@ad.faytechcc.edu>
2019-11-02 15:39:25 -0400

    Created a copy of Mob.

--- code/DungeonCrawl/ClassLibrary/Mob.cs
+++ code/DungeonCrawl/ClassLibrary/Mob.cs
@@ -8,7 +8,7 @@ namespace ClassLibrary
 {
     public class Mob: AliveObject
     {
-        public Mob(string description, string name, int hp, int ac, bool dead, string damage )
+        public Mob(string name, string description, int hp, int ac, bool dead, string damage )
                     : base(name, hp, ac, dead, damage)
         {
             Description = description;
@@ -25,6 +25,14 @@ namespace ClassLibrary
             {
                 Console.WriteLine($"{item.Name}");
             }
+        }
+
+        //Creates new instance of a mob without changing the original object.
+        public Mob NewMob()
+        {
+            Mob newEnemy = new Mob(Name, Description, HP, AC, Dead, Damage);
+
+            return newEnemy;
         }
     }
 }
